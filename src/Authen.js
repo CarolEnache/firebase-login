da84ad84b21778d00f5fd83bf0aaadc3cce5a089
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-09-26 14:10:04 +0100

    Setting up login calls

--- src/Authen.js
+++ src/Authen.js
@@ -12,11 +12,41 @@ var config = {
 firebase.initializeApp(config);
 
  class Authen extends Component {
+
+  login(event){
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+    console.log(email, password)
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(email, password);
+
+    // todo: handle login promise
+    promise.catch(e => {
+      var err = e.message;
+      console.log(err);
+      this.setState({err: err});
+   });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      err: ''
+    };
+    this.login = this.login.bind(this);
+    
+  }
+  
+
   render() {
     return (
       <div>
           <input id='email' ref='email' type='email' placeholder='Enter your email'/><br />
           <input id='pass' ref='password' type='password' placeholder='Enter your password'/><br />
+          <p>{this.state.err}</p>
+          <button onClick={this.login}>Login</button>
+          <button>Sign up</button>
+          <button>Log out</button>
       </div>
     )
   }
