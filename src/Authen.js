bd8913f70985a0958b740e8ee94dea181484bf57
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-09-26 15:04:39 +0100

    Google Sign In - setting up button

--- src/Authen.js
+++ src/Authen.js
@@ -66,6 +66,10 @@ firebase.initializeApp(config);
     lout.classList.add('hide');
   }
 
+  google(){
+    console.log('i am in google method')
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -74,6 +78,7 @@ firebase.initializeApp(config);
     this.login = this.login.bind(this);
     this.signup = this.signup.bind(this);
     this.logout = this.logout.bind(this);
+    this.google = this.google.bind(this);
   }
   
 
@@ -85,7 +90,8 @@ firebase.initializeApp(config);
           <p>{this.state.err}</p>
           <button onClick={this.login}>Login</button>
           <button onClick={this.signup}>Sign up</button>
-          <button onClick={this.logout} id='logout' className='hide'>Log out</button>
+          <button onClick={this.logout} id='logout' className='hide'>Log out</button><br />
+          <button onClick={this.google} id='google' className='google'>Sign In with Google</button><br />
       </div>
     )
   }
